ef2917ab58043557faa64f08769e20c48689250f
jonhetrick <jhetrick@seattleu.edu>
2020-01-27 15:55:12 -0700

    finished 1-fizzbuzz

--- 1-fizzbuzz/index.js
+++ 1-fizzbuzz/index.js
@@ -1,8 +1,8 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that counts from 1 to a given integer, printing out fizz if the number is divisble by 3, buzz if it is divisible by 5, and fizzbuzz if it is divisible by both 3 and 5.
+Input: Integer
+Output: list of numbers, with fizz is divisble by 3, buzz if divisible by 5, and fizzbuzz if divisble by both.
+Usage: Node 1-fizzbuzz <integer>
 */
 
 // get input using getargs
@@ -11,20 +11,20 @@ let input = getargs.getIntegerArg()
 
 // test that input is a number and exit with error
 // TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>")
 }
 else {
   // iterate from 1 to the value of input
   // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for (let i = 1; i<=input; i++) {
     
     //TODO provide for tests for divisible for 3,5, and 15 to create desired output
-    if ( ) {
+    if (i % 15 == 0) {
       console.log(`${i} fizzbuzz`);
-    } else if ( ) {
+    } else if ( i % 3 == 0) {
       console.log(`${i} fizz`);
-    } else if ( ) {
+    } else if (i % 5 == 0 ) {
       console.log(`${i} buzz`);
     } else {
       console.log(`${i}`);
