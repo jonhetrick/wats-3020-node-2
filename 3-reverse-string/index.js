49886a1510e052158164d3ef127acdd8137d2622
jonhetrick <jhetrick@seattleu.edu>
2020-01-27 16:15:45 -0700

    finish 3-reverse-string

--- 3-reverse-string/index.js
+++ 3-reverse-string/index.js
@@ -1,26 +1,20 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and reverses it using JavaScript string methods.
+Input: string 
+Output: string reversed
+Usage: Node 3-reverse-string <string>
 */
 
 // get input using getargs
-const getargs = require("../modules/getargs")
-let input = getargs.getStringArg()
-
+const getargs = require('../modules/getargs');
+let input = getargs.getStringArg();
 
 //test that it is a string
-//TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-    // TODO provide a usage statement
+//check if input is a string.
+if (!input) {
+	console.log('Node 3-reverse-string <string>');
+} else {
+	//splits the input string into individual items and then reverses them, followed by joining each item back together.
+	console.log(input.split('').reverse().join(''));
 }
-else {
-    // uses string methods to reverse the string
-    console.log(input.split('').reverse().join(''));
-}
-
-
-
-
