a27bf1a15b5e38b5a797ba0e9f6e2e4017aa1447
jonhetrick <jhetrick@seattleu.edu>
2020-01-27 18:23:23 -0700

    finished 5-character-count

--- 5-character-count/index.js
+++ 5-character-count/index.js
@@ -1,37 +1,40 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and counts how many of each character are in that string.
+Input: string 
+Output: list of the characters and how many of each. 
+Usage: 5-character-count
 */
 
 // get input using getargs
-const getargs = require("../modules/getargs")
-let input = getargs.getStringArg()
-
+const getargs = require('../modules/getargs');
+let input = getargs.getStringArg();
 
 //test that it is a string
-//TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-  // TODO provide a usage statement
+if (!input) {
+	console.log('usage: 5-character-count <string>');
+} else {
+	let charCount = {};
+	let characters = input.split('');
+
+	for (character of characters) {
+		if (charCount[character]) {
+			charCount[character]++;
+		} else {
+			charCount[character] = 1;
+		}
+	}
+
+	let charArr = [];
+	for (let character in charCount) {
+		let newElem = { char: character, count: charCount[character] };
+		charArr.push(newElem);
+	}
+	charArr.sort(function(a, b) {
+		return a.count - b.count;
+	});
+
+	for (let i = 0; i < charArr.length; i++) {
+		console.log(`${charArr[i].char}: ${charArr[i].count}`);
+	}
 }
-else {
-  // TODO initialize charCount object to count characters
-  // TODO turn the input into an array called characters using the split method
-  
-  // TODO iterate through array to create object that has character for key and count for value
-  
-  // TODO initialize a new array called charArr
-  // TODO loop through every character in charCount 
-  for ( ) {
-    let newElem = {char:character, count:charCount[character]};
-    // TODO add newElem to charArr using the push method
-  }
-
-  // TODO use the sort method of charArr with a custom compare function to sort the array by the count values (least to greatest)
-
-  // TODO output the content of the array to the console, showing each character and the count
-  
-}
-
