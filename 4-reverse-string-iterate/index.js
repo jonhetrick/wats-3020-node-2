6be6676e25232b270e006c0f139938bde767eb84
jonhetrick <jhetrick@seattleu.edu>
2020-01-27 17:22:38 -0700

    finished 4-reverse-string-iterate

--- 4-reverse-string-iterate/index.js
+++ 4-reverse-string-iterate/index.js
@@ -1,41 +1,34 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and reverses it by iterating through the string, character by character.
+Input: string 
+Output: reversed string 
+Usage: 4-reverse-string-iterate <string>
 */
 
 // get input using getargs
-const getargs = require("../modules/getargs")
-let input = getargs.getStringArg()
+const getargs = require('../modules/getargs');
+let input = getargs.getStringArg();
 
 function reverseWithFullIteration(str) {
-    let result = '';
-    // TODO loop from end of str (str.length-1) to beginning
-    for ( ) {
-        // TODO add the letter in the current position to the result string.
-    }
-    return result;
+	let result = '';
+	for (let element of str) {
+        result = element + result;
+	}
+	return result;
 }
 
-function reverseWithHalfIteration(str) {
-    // STRETCH TODO write a function that reverses the string but only iterates over half of the input string.
-}
+// function reverseWithHalfIteration(str) {
+// 	// STRETCH TODO write a function that reverses the string but only iterates over half of the input string.
+// }
 
 //test that it is a string
-//TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-    // TODO provide a usage statement
+if (!input) {
+	// usage statement
+	console.log('usage: 4-reverse-string-iterate');
+} else {
+	// use functions to reverse string
+	console.log('result with full iteration', reverseWithFullIteration(input));
+
+	// STRETCH TODO output the result of your reverseWithHalfIteration function
 }
-else {
-    // use functions to reverse string
-    console.log("result with full iteration", reverseWithFullIteration(input));
-
-    // STRETCH TODO output the result of your reverseWithHalfIteration function
-
-}
-
-
-
-
